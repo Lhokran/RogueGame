f6fe6ac3595d1969bb3c8680e5996b2db58cbe54
Sceleros <bertrandvaur@gmail.com>
2016-01-17 20:20:42 +0100

    Commit initial

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class MovingObject : MonoBehaviour {
+
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private float inverseMoveTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
